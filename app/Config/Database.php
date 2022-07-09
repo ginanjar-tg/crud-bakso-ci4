45da89c1a04de89dc8263f4078b8cf4013642e6c
ginanjartg <ginanjar0822@gmail.com>
2022-07-09 15:33:29 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'username' => 'ginc8711_admin',
+        'password' => 'Mikir123.',
+        'database' => 'ginc8711_crud_bakso',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
