49650a4d7e8a9f23bbb3deb29e38c82386dc845a
ginanjartg <ginanjar0822@gmail.com>
2022-07-09 15:29:19 +0700

    change icon

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,34 +1,30 @@
 <?php error_reporting(0); ?>
 
 <html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>CRUD Bakso</title>
-  </head>
-  <body>
-    <div class="h-screen w-screen flex flex-wrap m-auto">
-      <div class="m-auto text-center">
-        <h1 class="text-center text-2xl md:text-4xl text-black">
-          Selamat Datang di CRUD-Bakso <br />
-          Silakan Login Terlebih Dahulu
-        </h1>
-        <div class="py-32">
-          <a href="login">
-            <button
-              class="bg-blue-500 hover:bg-blue-700 text-2xl text-white font-bold py-8 px-32 md:px-32 rounded-full"
-            >
-              Login
-            </button>
-          </a>
-        </div>
-        <h1 class="text-center text-xl md:text-xl text-black">
-          By : Ginanjar Tubagus Gumilar <br />
-          10119032
-        </h1>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>CRUD Bakso</title>
+</head>
+
+<body>
+  <div class="h-screen w-screen flex flex-wrap m-auto">
+    <div class="m-auto text-center">
+      <h1 class="text-center text-2xl md:text-4xl text-black">
+        Selamat Datang di CRUD-Bakso <br />
+        Silakan Login Terlebih Dahulu
+      </h1>
+      <div class="py-32">
+        <a href="login">
+          <button class="bg-blue-500 hover:bg-blue-700 text-2xl text-white font-bold py-8 px-32 md:px-32 rounded-full">
+            Login
+          </button>
+        </a>
       </div>
     </div>
-  </body>
-</html>
+  </div>
+</body>
 
+</html>
